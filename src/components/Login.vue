2072e57f2ed57f1702713ed57de75e8c5a235e69
Jaime E Falla <65088216+Jaimedfalla@users.noreply.github.com>
2022-05-30 16:26:30 -0500

    Primeros pasos en el login

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="container">
+        <div class="form-container">
+            <h1 class="title">Login</h1>
+            <form @submit.prevent="login">
+                <div class="field">
+                    <label for="" class="label">Email</label>
+                    <div class="control">
+                        <input type="email" class="input" v-model="email" required>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
+</template>
+<script setup lang="ts">
+
+</script>
